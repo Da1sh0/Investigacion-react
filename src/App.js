4cc079ef15385c4a5e342a8b97762808837659fa
Diiego <dieguitokmino@gmail.com>
2024-03-02 23:43:05 -0500

    Empezando Investigacion y practica con react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Diiego";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MostraeNombreComponente />
     </div>
   );
+}
+
+const mostraeNombre =  () => {
+  return "Diiego"
+}
+
+const MostraeNombreComponente =  () => {
+  return <h1>Diiego</h1>
 }
 
 export default App;
